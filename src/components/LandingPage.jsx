cc8e489160a60f9a09f48e98f61b0b775161aeb4
sandip chakraborty <unicornsc77@gmail.com>
2025-09-18 12:00:40 +0530

    fixed grid design..

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -500,7 +500,7 @@ function LandingPage() {
                       whileTap={{ scale: 1 }}
                       transition={{ duration: 0.2 }}
                       key={v._id}
-                      className="relative bg-gray-800 rounded-xl p-2 flex flex-col items-center min-w-[2/3] sm:min-w-[3/4] lg:min-w-[4/5]"
+                      className="relative bg-gray-800 rounded-xl p-2 flex flex-col items-center min-w-[20em]"
                     >
                       <button
                         onClick={() => toggleRead(v)}
